fda26ca27fdb5edb1f4746f754931b602d867bd2
Hyunie Yeom <75717579+Gyeong-Hyeon@users.noreply.github.com>
2022-09-30 21:07:38 +0900

    Time: 417 ms (49.66%), Space: 15.5 MB (73.28%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -1,13 +1,15 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        min_idx, max_idx = 0, len(nums)-1
-        while min_idx <= max_idx:
+        min_idx, max_idx = 0, len(nums)
+        min_idx = 0
+        while True:
             mid_idx = (max_idx+min_idx)//2
             if nums[mid_idx] == target:
                 return mid_idx
+            if max_idx - min_idx <= 1:
+                return -1
             elif nums[mid_idx] < target:
-                min_idx = mid_idx+1
+                min_idx = mid_idx
                 continue
             else:
-                max_idx = mid_idx-1
-        return -1
+                max_idx = mid_idx
