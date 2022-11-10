173d624483e62c6cfda692bd31e7739c984e7370
Hyunie Yeom <75717579+Gyeong-Hyeon@users.noreply.github.com>
2022-11-10 23:16:35 +0900

    Time: 548 ms (54.45%), Space: 15.4 MB (73.54%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,15 +1,21 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        max_idx = len(nums)
-        min_idx = 0
-        while True:
-            mid_idx = int((max_idx+min_idx)/2)
-            if nums[mid_idx] == target:
-                return mid_idx
-            if max_idx - min_idx <= 1:
-                return -1
-            elif nums[mid_idx] < target:
-                min_idx = mid_idx
-                continue
+        """
+        1. Set the first searching area from the first value of the input array and the last value of the array
+        2. If the target is same with the middle value of the searching area, return the middle value's index
+        3. If the target is smaller than the middle value of the searching area, move "high" to the right left value of the middle value of the searching area
+        4. If the target is bigger than the middle value of the searching area, move "low" to the right right middle value of the searching area
+        5. Repeat 2~4 to narrow down the searching area
+        6. If target is not found until searching area has one value only, return -1
+        """
+        if not nums: return -1
+        low, high = 0, len(nums)-1
+        while low <= high:
+            mid = (low+high)//2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] > target:
+                high = mid-1
             else:
-                max_idx = mid_idx
+                low = mid+1
+        return -1            
