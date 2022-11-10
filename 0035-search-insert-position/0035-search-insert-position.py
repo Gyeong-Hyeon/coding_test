3ecf227ce7c8f621a83c4ff4e3a8d6b4f47a8f62
Hyunie Yeom <75717579+Gyeong-Hyeon@users.noreply.github.com>
2022-11-11 02:33:21 +0900

    Time: 94 ms (65.96%), Space: 14.8 MB (39.77%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -2,8 +2,17 @@ class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         """
         1. Set the search array same with the input array
-        2. If the middle value is same with target, return the indext of the middle value
-        3. If the target is not found until the search array == False, return high+1
+        2. If the middle value is same with target, return the index of the middle value
+        3. If the target is not found until low gets bigger than high, return low
+        While statement continues to narrow down the searching array untill the array is either:
+            Case 1. 2 values in the searching array:
+                - "high" is pointing the last index of searching array
+                - "low" and "mid" is pointing the first index of searching array
+            Case 2. 1 value in the searching array:
+                - "high", "low", "mid" is pointing a same index
+            That means the last comparing steps is actually comparing "low" value and the target (because "mid" value is same with "low" value).
+            If the target value is bigger than "low" value, the while statement excuted once more because "low" becomes same with "high".
+            Else(=that means the target value is smaller than "low"), "high" becomes smaller than "low"(because of mid-1) and while statement terminates. The index where the target would be is where "low" value is (because it's smaller than "low"), so, return "low".
         """
         low, high = 0, len(nums)-1
         while low <= high:
