aa223ad3b0e44ea9e576aacfd41b7d103275bcaf
Hyunie Yeom <75717579+Gyeong-Hyeon@users.noreply.github.com>
2022-11-11 22:13:21 +0900

    Time: 640 ms (19.47%), Space: 16.3 MB (49.01%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,14 +1,29 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        output = [0] * len(nums)
-        start, end = 0, len(nums) - 1
-        while start <= end:
-            s_start, s_end = nums[start]**2, nums[end]**2
-            if s_start > s_end:
-                output[end - start] = s_start
-                start+=1
+        """
+        * Why the description says "non-decreasing" order, not "increasing" order?
+         - Maybe there could be same numbers
+         1) If the first value is neg, it's the biggest val among all neg vals in the array after squaring.
+         2) The last value is the biggest val among all pos vals in the array
+         3) We need to compare two values and move pointers based on the result of comparing. So need to use Two pointer algorithm
+         
+         1. Set left = 0, right = last value idx, idx = new array idx, first value is same with right
+         2. Compare the square of left and the square of right
+            - If left > right, put the square of left in the new array[idx] and left+1
+            - Else, put the squre of right in the new array[idx] and right -1
+            - idx-1
+         3. Repeat 2 until idx == 0. If idx == -1, return the new array.
+        """
+        ans, left, right, idx = [0]*len(nums), 0, len(nums)-1, len(nums)-1
+        left_val, right_val = nums[left]**2, nums[right]**2
+        while idx >= 0:
+            if left_val > right_val:
+                ans[idx] = left_val
+                left+=1
+                left_val = nums[left] **2
             else:
-                output[end - start] = s_end
-                end-=1
-        return output
-    
+                ans[idx] = right_val
+                right-=1
+                right_val = nums[right]**2
+            idx-=1
+        return ans
