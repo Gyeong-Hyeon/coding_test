b2655020b001cdc38013739d2c984063e96db97c
Hyunie Yeom <75717579+Gyeong-Hyeon@users.noreply.github.com>
2022-11-13 22:29:08 +0900

    Time: 140 ms (17.42%), Space: 14.4 MB (99.71%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.py
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.py
@@ -1,3 +1,27 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        return ' '.join([x[::-1] for x in s.split()])
+        """
+        1) Pythonic way
+            1. Split the string by empty space and put each word into a list
+            2. Rotate the list
+            3. Reverse each word
+            4. After rotation, join the values of list with space
+            Full code = return ' '.join([x[::-1] for x in s.split()])
+        2) Two pointers
+            1. set t='', ans=[]
+            2. Rotate the string
+            3. If value == ' ', ans.append[t], t='' 
+            4. Else, value+t
+            5. After rotation, append t once more in case there wasn't whiespace at the end of "s"
+            6. join the values of list with space
+        """
+        t, ans = '', []
+        for st in s:
+            if st == ' ':
+                ans.append(t)
+                t = ''
+                continue
+            t=st+t
+        if t:
+            ans.append(t)
+        return ' '.join(ans)
