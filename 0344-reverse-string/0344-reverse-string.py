17f2807a06739eae3aa7f8bd42b3be0aab1c0023
Hyunie Yeom <75717579+Gyeong-Hyeon@users.noreply.github.com>
2022-11-13 22:05:17 +0900

    Update 0344-reverse-string.py

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -2,6 +2,13 @@ class Solution:
     def reverseString(self, s: List[str]) -> None:
         """
         Do not return anything, modify s in-place instead.
+        1. We can use python list function
+            s[:] = s[::-1]
+        2. We can use two pointer algorithm
+          1. Set first point and last point
+          2. Swap the value of each point
+          3. first move one to right, last move one to left
+          4. Repeat 2 until first == last
         """
         l = len(s)-1
         for i in range(len(s)//2):
