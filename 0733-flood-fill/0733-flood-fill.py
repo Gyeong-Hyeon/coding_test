b4d9adec545122e68268151eb34e1e8f0c037008
Gyeong-Hyeon <kyunghyun7843@gmail.com>
2022-11-16 23:30:10 +0900

    Time: 150 ms (53.66%), Space: 14.1 MB (40.08%)

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,11 +1,26 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        def change(r,c):
-            if r < 0 or c < 0 or r == len(image) or c == len(image[0]) or image[r][c] != s:
-                return 0
+        """
+        1. set variable num to current image[sr][sc]'s value
+        2. Define a function "change_color" doing:
+            - parameter: row_idx, col_idx
+            - Return False if one of the following case is true:
+                - row_idx < 0
+                - row_idx >= len(image)
+                - col_idx < 0
+                - col_idx >= len(image[0])
+                - image[row_idx][col_idx] != num
+            - Else, change image[row_idx][col_idx]'s value to num
+            - call function change_color(row_idx-1, col_idx) change_color(row_idx+1, col_idx) change_color(row_idx, col_idx-1) change_color(row_idx, col_idx-1)
+        3. if num != color: change_color(sr,sc)
+        """
+        org = image[sr][sc]
+        if org == color:
+            return image
+        def change_color(r,c):
+            if r < 0 or r >= len(image) or c < 0 or c >= len(image[0]) or image[r][c] != org:
+                return False
             image[r][c] = color
-            return change(r-1, c), change(r+1,c), change(r,c-1), change(r,c+1)        
-        s = image[sr][sc]
-        if s != color:
-            change(sr,sc)
+            return change_color(r-1,c), change_color(r+1,c), change_color(r,c-1), change_color(r,c+1)
+        change_color(sr,sc)
         return image
