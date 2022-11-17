9ebab17794da04ed5d0cfb4b0984cc47777b69f7
Hyunie Yeom <75717579+Gyeong-Hyeon@users.noreply.github.com>
2022-11-18 00:15:33 +0900

    Time: 166 ms (13.40%), Space: 15.6 MB (77.52%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.py
@@ -10,14 +10,28 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        if not root:
-            return root
-        q = [root]
-        while q:
-            nn = None
-            for _ in range(len(q)):
-                n = q.pop(0)
-                n.next, nn = nn, n
-                if n.right:
-                    q.extend([n.right, n.left])
-        return root
+        """
+        1. visit list = [root.left, root.right], prev_node = False
+        2. for _ in visit list's length:
+            if not prev_node:
+                prev_node = visit list.pop(0)
+            else:
+                node = visit list.pop(0)
+                prev_node.next, prev_node = node, node
+            if prev_node.left:
+                visit list.extend([prev_node's left node, right node])
+        5. if not visit list: break the loop
+        """
+        if not root: return root
+        v = [root]
+        while v:
+            p_node = False
+            for _ in range(len(v)):
+                if not p_node:
+                    p_node = v.pop(0)
+                else:
+                    node = v.pop(0)
+                    p_node.next, p_node = node, node
+                if p_node.left:
+                    v.extend([p_node.left, p_node.right])
+        return root        
