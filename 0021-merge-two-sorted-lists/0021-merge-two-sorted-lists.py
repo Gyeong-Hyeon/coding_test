61f861ce99168f060637a623156ad4d8dc821048
Hyunie Yeom <75717579+Gyeong-Hyeon@users.noreply.github.com>
2022-11-20 20:09:44 +0900

    Time: 54 ms (77.21%), Space: 14 MB (33.28%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,16 +5,28 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if not list1 or not list2:
-            return list2 or list1
-        ans = move = ListNode()
-        while list1 and list2:
+        """
+        Make a new linked list
+        1. list1.val < list2.val: node.val = list1.val, move node and list1 to next
+        2. else: node.val = list2.val, move node and list2 to next
+        3. If list1 or list2 reached to each's last node, make node.next.next to the other linked list
+        """
+        if not list1 or not list2: return list2 or list1
+        merged = node = ListNode()
+        while list1 or list2:
             if list1.val < list2.val:
-                move.next = list1
-                move, list1 = list1, list1.next
-                continue
-            move.next = list2
-            move, list2 = move.next, list2.next
-        if list1 or list2:
-            move.next = list1 if list1 else list2
-        return ans.next
+                node.next = list1
+                if not list1.next:
+                    node.next.next = list2
+                    break
+                else:
+                    list1 = list1.next
+            else:
+                node.next = list2
+                if not list2.next:
+                    node.next.next = list1
+                    break
+                else:
+                    list2 = list2.next
+            node = node.next
+        return merged.next        
