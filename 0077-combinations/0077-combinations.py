7d91bbab92fe731da8716bf970accdbd3d466732
Hyunie Yeom <75717579+Gyeong-Hyeon@users.noreply.github.com>
2022-11-21 00:12:19 +0900

    Time: 202 ms (85.65%), Space: 16.1 MB (18.84%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,16 +1,21 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        def dfs(nums, path, k, ret):
-            if len(path) == k:
-                ret.append(path)
-                return
-            
-            for i in range(len(nums)):
-                if len(nums)-i+len(path)>=k:  # Notice Here
-                    dfs(nums[i+1:], path+[nums[i]], k, ret)
-        
-        r = []
-        dfs(range(1, n+1), [], k, r)
-        
-        return r
-        
+        """
+        Pascal's Identity - nCk = n-1Ck + n-1Ck-1
+        So nCr will be n-1Ck + adding n to each combination of n-1Ck-1.
+        Therefore, we can devide this problem in 4small problems.
+        1. When k == 0: Retuns an empty list since the number of elements in the combination is 0.
+        2. When k == 1: Each element in the list is a combination, the number of combination is n.
+        3. When n == k: 1~n will be one combination. The number of combination is 1.
+        4. Else, call n-1Ck and n-1Ck-1 appended n to the each combination
+        """
+        if k == 0:
+            return []
+        if k == 1:
+            return [[i] for i in range(1, n+1)]
+        if n == k:
+            return [[i for i in range(1, n+1)]]
+        ans = self.combine(n-1,k-1)
+        ans = [comb + [n] for comb in ans]
+        ans.extend(self.combine(n-1,k))
+        return ans
