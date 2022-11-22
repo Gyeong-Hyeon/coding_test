af48268bab659f34abd6ea3f2cf0aebf85cd4bdb
Hyunie Yeom <75717579+Gyeong-Hyeon@users.noreply.github.com>
2022-11-23 00:59:58 +0900

    Time: 43 ms (80.61%), Space: 13.9 MB (54.28%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -1,13 +1,16 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         """
+        - Basic
         1. if n <= 0 not power of 2, 1 and 2 is power of 2
         2. from 3, bit(n) starts with 1 and the other digits are 0 when n is a power of 2.
+        
+        - Using bit manipulation
+        1. When n is a power of 2, n is 1000...0 and n-1 is 111...1, len(bit(n))-len(bit(n-1)) is 1
+            Therefore, when we do n&n-1, it will always return False when n is a power of 2 because n&n-1 is 0
+            On the other hand, when n is not a power of 2, len(bit(n)) = len(bit(n-1)) so n&n-1 > 0 == True.
+            Also, n == True when n != 0.
+        2. When n is 0, n == False.
+        3. In conculsion, we can return n and not (n&n-1). It will be True when n is a power of 2, False when n is not a power of 2.
         """
-        if n <= 0:
-            return False
-        if n <= 2:
-            return True
-        bit = bin(n)[2:]
-        if bit[0] == '1' and set(bit[1:]) == {'0'}: return True
-        else: return False
+        return n and not (n&n-1)
