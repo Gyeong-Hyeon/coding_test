628cd541ed9ddbcbf08183cc7b9060f9878e7064
Hyunie Yeom <75717579+Gyeong-Hyeon@users.noreply.github.com>
2022-11-23 00:33:57 +0900

    Time: 60 ms (53.06%), Space: 13.9 MB (9.87%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -1,4 +1,13 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        return n and not (n & n - 1)
-        
+        """
+        1. if n <= 0 not power of 2, 1 and 2 is power of 2
+        2. from 3, bit(n) starts with 1 and the other digits are 0 when n is a power of 2.
+        """
+        if n <= 0:
+            return False
+        if n <= 2:
+            return True
+        bit = bin(n)[2:]
+        if bit[0] == '1' and set(bit[1:]) == {'0'}: return True
+        else: return False
