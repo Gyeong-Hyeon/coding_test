18f77876bfd42c320517067faf9bdffbf0bd6a96
Hyunie Yeom <75717579+Gyeong-Hyeon@users.noreply.github.com>
2022-11-23 01:44:55 +0900

    Time: 66 ms (25.32%), Space: 13.9 MB (50.38%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        """
+        1. Bit manipulation
+            n&n-1 manipulation replaces the rightmost 1 in n with a 0 and leaves the remaining 1s and 0s
+            because n is (000...)1...00...1 and n-1 is (000...)1...00...0 so the result is (000...)1...00...0.
+            Therefore, when we repeat the manipulation and add 1 to count each time, the value of count becomes the number of 1 when n becomes 0.
+        2. Use python function
+            n.bit_count()
+        """
+        return n.bit_count()
