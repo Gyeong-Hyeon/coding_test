45a1ed6a7f9d8905927f6de912daa209df1a5c18
Gyeong-Hyeon <kyunghyun7843@gmail.com>
2022-12-19 22:29:33 +0900

    Time: 230 ms (11.27%), Space: 25.5 MB (69.87%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -2,8 +2,18 @@ class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
+        1.  Make k to remainder after divided by len(nums) because if k is multiple of len(nums), it means full rotation, nums is same after rotation.
+        The array after rotation is same with below:
+        1) Reverse the array
+        2) Reverse the array[:k] + reverse the array[-k:]
+        
+        Another solution: nums[-k:] + nums[:-k]
         """
-        k=k%len(nums) 
-        if k==0 or k==len(nums):
-            return nums
-        nums[:]=nums[-k:]+nums[:len(nums)-k]
+        l = len(nums)
+        k%=l
+        if k == 0: pass
+        nums.reverse()
+        for i in range(k//2):
+            nums[i], nums[k-i-1] = nums[k-i-1], nums[i]
+        for i in range((l-k)//2):
+            nums[k+i], nums[l-i-1] = nums[l-i-1], nums[k+i]
