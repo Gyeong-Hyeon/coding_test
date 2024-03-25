3b983a860981a3989d4997ae123fd134d8530faa
Hyunie Yeom <75717579+Gyeong-Hyeon@users.noreply.github.com>
2024-03-25 20:40:56 +0900

    Runtime: 461 ms, faster than 98.62% of Python3 online submissions for 3Sum. Memory Usage: 20.8 MB, less than 25.71% of Python3 online submissions for 3Sum.

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,19 +1,26 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        ans, n = [], len(nums)
-        for i in range(n):
-            if i > 0 and nums[i] == nums[i-1]:
+        l, ans = len(nums), []
+        for p1 in range(l-2):
+            if p1 > 0 and nums[p1] == nums[p1-1]:
                 continue
-            target, start, end = nums[i]*-1, i+1, n-1
-            while start < end: #start to find target
-                if nums[start] + nums[end] == target:
-                    ans.append([nums[i],nums[start],nums[end]])
-                    start+=1
-                    while start < end and nums[start] == nums[start-1]:
-                        start+=1
-                elif nums[start] + nums[end] < target:
-                    start+=1
-                else:
-                    end-=1
-        return ans   
+            if nums[p1] > 0:
+                break
+            target, p2, p3 = -nums[p1], p1+1, l-1
+            while p2 < p3:
+                s = nums[p2] + nums[p3]
+                if s < target:
+                    p2+=1
+                    continue
+                if s > target:
+                    p3-=1
+                    continue
+                ans.append([nums[p1], nums[p2], nums[p3]])
+                p2+=1
+                p3-=1
+                while p2 < p3 and nums[p2] == nums[p2-1]:
+                    p2+=1
+                while p2 < p3 and nums[p3] == nums[p3+1]:
+                    p3-=1
+        return ans
