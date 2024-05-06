77ca30b855bbd2bd4fce7a2f171c1dc30b085b5b
Hyunie Yeom <75717579+Gyeong-Hyeon@users.noreply.github.com>
2024-05-06 22:45:52 +0900

    Time: 491 ms (5.02%), Space: 18.3 MB (53.53%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,24 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        schedule = {}
+        for s, f in prerequisites:
+            if s == f:
+                return False
+
+            nodes = schedule.get(s, [])[:]
+            if not nodes:
+                schedule[f] = schedule.get(f,[]) + [s]
+                continue
+
+            while nodes:
+                node = nodes.pop(0)
+                if f == node:
+                    return False
+                to_visit = schedule.get(node)
+                if not to_visit:
+                    continue
+                if f in to_visit:
+                    return False
+                nodes.extend(to_visit)
+            schedule[f] = schedule.get(f,[]) + [s]
+        return True
