3e1a49d18f31e38aca511b27b440fca8762b3fe4
Hyunie Yeom <75717579+Gyeong-Hyeon@users.noreply.github.com>
2024-05-23 21:04:46 +0900

    Time: 33 ms (87.08%), Space: 16.6 MB (65.14%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,9 +3,7 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        n = len(matrix)
-        cnt = n//2
-        
+        n, cnt = len(matrix)-1, len(matrix)//2
         for i in range(cnt+1):
-            for j in range(i, n-1-i):
-                matrix[j][n-1-i], matrix[n-1-i][n-1-j], matrix[n-1-j][i], matrix[i][j] = matrix[i][j], matrix[j][n-1-i], matrix[n-1-i][n-1-j], matrix[n-1-j][i]
+            for j in range(i, n-i):
+                matrix[j][n-i], matrix[n-i][n-j], matrix[n-j][i], matrix[i][j] = matrix[i][j], matrix[j][n-i], matrix[n-i][n-j], matrix[n-j][i]
