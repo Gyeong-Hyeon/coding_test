0be533265211dc5b5926939856fe3a38afe008fe
Hyunie Yeom <75717579+Gyeong-Hyeon@users.noreply.github.com>
2024-05-23 21:02:28 +0900

    Time: 34 ms (82.69%), Space: 16.6 MB (19.64%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -9,4 +9,3 @@ class Solution:
         for i in range(cnt+1):
             for j in range(i, n-1-i):
                 matrix[j][n-1-i], matrix[n-1-i][n-1-j], matrix[n-1-j][i], matrix[i][j] = matrix[i][j], matrix[j][n-1-i], matrix[n-1-i][n-1-j], matrix[n-1-j][i]
-                print(matrix)
