a5d2bd7bb533acaba79a40ac2372da1a6f89d176
Hyunie Yeom <75717579+Gyeong-Hyeon@users.noreply.github.com>
2024-05-27 20:57:49 +0900

    Time: 93 ms (99.45%), Space: 17.7 MB (74.49%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,9 +1,5 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        cnts = [False]*(len(nums)+1)        
-        for num in nums:
-            cnts[num] = True
-        
-        for i, cnt in enumerate(cnts):
-            if not cnt:
-                return i
+        target = len(nums)*(len(nums)+1)/2
+        input_sum = sum(nums)
+        return int(target-input_sum)
