9f5e73d9a2c4128b42491b43329400f56eef417e
Hyunie Yeom <75717579+Gyeong-Hyeon@users.noreply.github.com>
2024-06-09 21:39:42 +0900

    Runtime: 40 ms, faster than 30.13%/ Memory Usage: 16.4 MB, less than 82.82%

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -5,21 +5,12 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        """
-        1. Let fast pointer run the linked list by index n-1
-        2. When fast pointer arrives nth idx, slow pointer starts
-        3. when fast pointer arrive end of the list, slow pointer stops
-        4. The total length fast runner run was len(linked list). The first length the fast runner run was n-1. So the second length the fast runner run was len(linked list) - n-1 which is same with the slow pointer run. Therefore, the index where slow pointer points is n-1th from the end of the list.
-        5. Make current slow runner.next to slow runner.next.next
-        6. return head
-        """
-        fast, slow = head, head
+        fast = head
+        slow = node = ListNode(next=head)
         for _ in range(n):
             fast = fast.next
-        if not fast:
-            return head.next
-        while fast.next:
+        while fast:
             fast = fast.next
             slow = slow.next
         slow.next = slow.next.next
-        return head
+        return node.next
