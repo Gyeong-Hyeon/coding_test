59f48cae139e4d20bd02d8dc381e6b279080221f
Hyunie Yeom <75717579+Gyeong-Hyeon@users.noreply.github.com>
2024-06-09 19:47:05 +0900

    Runtime: 41 ms, faster than 72.79%/ Memory Usage: 16.9 MB, less than 86.29%

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,16 +1,21 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        start, end = 0, len(nums)-1
-        while start <= end:
-            mid = (start+end)//2
-            if nums[mid] == target:
-                return mid
-            if nums[start] > nums[end]:
-                if nums[start] == target:
-                    return start
-                start+=1
-            elif nums[mid] < target:
-                start = mid+1
+        left, right = 0, len(nums)-1
+        while left <= right:
+            mid = (left+right)//2
+            if nums[mid] > nums[-1]:
+                left = mid+1
             else:
-                end = mid-1
-        return -1
+                right = mid-1
+
+        def binarySearch(sub_array:list, pivot:int=0):
+            sub_left, sub_right = 0, len(sub_array)-1
+            while sub_left <= sub_right:
+                sub_mid = (sub_left+sub_right)//2
+                if sub_array[sub_mid] == target:
+                    return sub_mid + pivot
+                if sub_array[sub_mid] < target:
+                    sub_left = sub_mid+1
+                else:
+                    sub_right = sub_mid-1
+            return -1
