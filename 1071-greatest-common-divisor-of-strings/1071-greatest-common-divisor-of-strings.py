71e57ce973844b2be72105b9946bf8669a7b39d7
Hyunie Yeom <75717579+Gyeong-Hyeon@users.noreply.github.com>
2024-12-07 12:00:05 +0900

    Time: 0 ms (100.00%), Space: 17.3 MB (6.52%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -1,8 +1,12 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
+        #유클리디안 호제법
+        def gcd(x,y):
+            while(y):
+                x,y = y,x%y
+            return x
+
         if str1+str2 != str2+str1:
             return ''
-        len1, len2 = len(str1), len(str2)
-        for i in range(min(len1, len2), 0 , -1):
-            if len1%i == 0 and len2%i == 0:
-                return str1[:i]
+        i = gcd(len(str1), len(str2))
+        return str1[:i]
