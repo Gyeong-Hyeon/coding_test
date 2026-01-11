dc69d5646907371c6ae3dcd74d7091a65bcb64ef
Hyunie Yeom <75717579+Gyeong-Hyeon@users.noreply.github.com>
2026-01-12 01:05:52 +0900

    0022-GENERATE-PARENTHESES Stats: Time: 5 ms (8.34%), Space: 12.8 MB (48.78%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -4,27 +4,26 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
-        node = {
-            'opened': 1,
-            'closed': 0,
-            'comb': '('
-        }
-        ans = []
-        to_visit = [None]
-
-        while node:
-            opened, closed = node['opened'], node['closed']
-            if opened == n:
-                comb = node['comb'] + (')'*(opened-closed))
-                ans.append(comb)
-                node = to_visit.pop()
-                continue
+        #ì¬ê·ë¥¼ ì´ì©í ë°©ë²
+        ans, total_length = [], n*2
+        def back_track(cur_str, opened, closed):
+            #ì ì²´ ê´í¸ ìë¥¼ ë¤ ì¬ì©í ê²½ì°
+            if len(cur_str) == total_length:
+                ans.append(''.join(cur_str))
+                return
+            
+            #ìì§ ì´ ì ìë ê´í¸ê° ë¨ììë ê²½ì°
+            if opened < n:
+                cur_str.append('(')
+                back_track(cur_str, opened+1, closed)
+                #ë¤ì ì°ì°ì ìí´ ì¶ê°í strì ê±°
+                cur_str.pop()
+            
+            #ìì§ ë«ì ì ìë ê´í¸ê° ë¨ììë ê²½ì°
             if closed < opened:
-                to_visit.append({
-                    'opened': opened,
-                    'closed': closed+1,
-                    'comb': node['comb'] + ')'
-                })
-            node['opened']+=1
-            node['comb']+='('
+                cur_str.append(')')
+                back_track(cur_str, opened, closed+1)
+                cur_str.pop()
+        
+        back_track([],0,0)
         return ans
