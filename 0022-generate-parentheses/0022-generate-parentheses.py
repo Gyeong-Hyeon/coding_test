048516726a382fef1036298624d85937bdbca3b7
Hyunie Yeom <75717579+Gyeong-Hyeon@users.noreply.github.com>
2026-01-12 01:12:29 +0900

    0022-GENERATE-PARENTHESES Stats: Time: 2 ms (51.49%), Space: 12.6 MB (79.09%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -4,20 +4,20 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
-        #ì¬ê·ë¥¼ ì´ì©í ë°©ë²
-        ans, total_length = [], n*2
+        #ì¬ê·ë¥¼ ì´ì©í ë°©ë² (+pruning)
+        ans = []
         def back_track(cur_str, opened, closed):
-            #ì ì²´ ê´í¸ ìë¥¼ ë¤ ì¬ì©í ê²½ì°
-            if len(cur_str) == total_length:
-                ans.append(''.join(cur_str))
+            #ë ì´ì ì´ ê´í¸ê° ìë ê²½ì°
+            if opened == n:
+                remained = ')' * (n-closed)
+                ans.append(''.join(cur_str) + remained)
                 return
             
             #ìì§ ì´ ì ìë ê´í¸ê° ë¨ììë ê²½ì°
-            if opened < n:
-                cur_str.append('(')
-                back_track(cur_str, opened+1, closed)
-                #ë¤ì ì°ì°ì ìí´ ì¶ê°í strì ê±°
-                cur_str.pop()
+            cur_str.append('(')
+            back_track(cur_str, opened+1, closed)
+            #ë¤ì ì°ì°ì ìí´ ì¶ê°í strì ê±°
+            cur_str.pop()
             
             #ìì§ ë«ì ì ìë ê´í¸ê° ë¨ììë ê²½ì°
             if closed < opened:
